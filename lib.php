e3e6e9339d9e40dc3947f29bd906483351d0c7fd
TommyTeaVee <fihlatv@gmail.com>
2026-05-26 16:34:54 +0200

    replaced the portfolio access control

--- lib.php
+++ lib.php
@@ -2,15 +2,25 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-function local_poe_extend_navigation_course(navigation_node $navigation, stdClass $course, context_course $context) {
+function local_poe_extend_navigation_course(
+    navigation_node $navigation,
+    stdClass $course,
+    context_course $context
+) {
 
-    $url = new moodle_url('/local/poe/course.php', ['id' => $course->id]);
+    if (
+        has_capability('moodle/course:update', $context)
+        || has_capability('moodle/site:config', $context)
+    ) {
 
-    $navigation->add(
-        'Portfolio of Evidence',
-        $url,
-        navigation_node::TYPE_COURSE,
-        null,
-        'Portfolio of Evidence'
-    );
+        $url = new moodle_url(
+            '/local/poe/course.php',
+            ['id' => $course->id]
+        );
+
+        $navigation->add(
+            'Portfolio of Evidence',
+            $url
+        );
+    }
 }
